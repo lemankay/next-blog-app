3162fe8adb50e01a0e36911620485f7a1fa2baf3
lemankay <46055728+lemankay@users.noreply.github.com>
2023-06-16 17:30:01 +0300

    Update TheHeader.tsx

--- components/TheHeader.tsx
+++ components/TheHeader.tsx
@@ -8,6 +8,15 @@ const TheHeader = () => {
           <div className="logo"><h2>Your Logo</h2></div>
             <div className="nav" >
                     <header>
+                      <!-- Google tag (gtag.js) -->
+                          <script async src="https://www.googletagmanager.com/gtag/js?id=G-HN8G5B01H4"></script>
+                          <script>
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                          
+                            gtag('config', 'G-HN8G5B01H4');
+                          </script>
                         <Link href='/'>Home</Link>
                         <Link href='/blog'>Blog</Link>
                         <Link href='/about'>About</Link>
